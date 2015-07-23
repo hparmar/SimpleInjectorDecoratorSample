0c415dbb792b5c4d48e183be2dbfcb22ce6c126a
Nuri YILMAZ <dengere@gmail.com>
2015-07-23 20:28:13 +0300

    starting up

--- SimpleInjectorDecoratorSample/Program.cs
+++ SimpleInjectorDecoratorSample/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleInjector;
+using SimpleInjector.Extensions;
 
 namespace SimpleInjectorDecoratorSample
 {
@@ -10,6 +12,52 @@ namespace SimpleInjectorDecoratorSample
     {
         static void Main (string[] args)
         {
+            var container = new Container();
+            container.Register<ISomethingHandler, SimpleSomethingHandler>();
+            container.RegisterDecorator(
+                typeof(ISomethingHandler),
+                typeof(DoSomethingHandler),
+                ctx => ShouldDecorate(ctx.ServiceType));
+            container.Verify();
+
+            container.GetInstance<ISomethingHandler>().Handle();
+        }
+
+        private static bool ShouldDecorate (Type serviceType)
+        {
+#error koşulu değiştirerek dene. change condition for different result.
+            if (serviceType == typeof(SimpleSomethingHandler))
+                return true;
+            return false;
+        }
+    }
+
+    public interface ISomethingHandler
+    {
+        void Handle ();
+    }
+
+    public class SimpleSomethingHandler : ISomethingHandler
+    {
+        public void Handle ()
+        {
+            Console.WriteLine("Simple handle executing...");
+        }
+    }
+
+    public class DoSomethingHandler : ISomethingHandler
+    {
+        private readonly ISomethingHandler _decorated;
+
+        public DoSomethingHandler (ISomethingHandler decorated)
+        {
+            _decorated = decorated;
+        }
+
+        public void Handle ()
+        {
+            Console.WriteLine("Do handle executing...");
+            _decorated.Handle();
         }
     }
 }
